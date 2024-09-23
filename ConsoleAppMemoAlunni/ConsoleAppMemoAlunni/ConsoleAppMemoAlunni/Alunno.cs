0f4067cb9c382ef22ba35a2d14b32866c810a090
FabioFant <fabiofanta07@gmail.com>
2024-09-23 17:24:53 +0200

    Aggiunto un lavoro

---
+++ ConsoleAppMemoAlunni/ConsoleAppMemoAlunni/ConsoleAppMemoAlunni/Alunno.cs
@@ -0,0 +1,37 @@
+﻿// Fabio Fantini 4H 2024-09-20
+// Classe Alunno
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMemoAlunni
+{
+    internal class Alunno
+    {
+        private string   nome;
+        private string   cognome;
+        private string   genere;
+        private DateOnly dataDiNascita;
+        private string   classe;
+        private string   indirizzo;
+
+        public Alunno(string nome, string cognome, string genere, DateOnly dataDiNascita, string classe, string indirizzo)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.genere = genere;
+            this.dataDiNascita = dataDiNascita;
+            this.classe = classe;
+            this.indirizzo = indirizzo;
+        }
+        public string   GetNome()          {  return nome; }
+        public string   GetCognome()       { return cognome; }
+        public string   GetGenere()        { return genere; }
+        public DateOnly GetDataDiNascita() { return dataDiNascita; }
+        public string   GetClasse()        { return classe; }
+        public string   GetIndirizzo()     { return indirizzo; }
+    }
+}
---
+++ ConsoleAppMemoAlunni/ConsoleAppMemoAlunni/ConsoleAppMemoAlunni/Program.cs
@@ -0,0 +1,64 @@
+﻿// Fabio Fantini 4H 2024-09-20
+// Memorizzazione di 200 Alunni con l'utilizzo di una classe
+
+namespace ConsoleAppMemoAlunni
+{
+    internal class Program
+    {
+        const string        file_input = @"..\..\..\input.txt";
+        static List<Alunno> alunni = new List<Alunno>();
+        static int          numeroAlunno = 0; // Usato per individuare l'alunno durante la lettura dell'input
+
+        static void LeggiAlunni()
+        {
+            using (StreamReader sr = new StreamReader(file_input))
+            {
+                while (!sr.EndOfStream)
+                {
+                    // Lettura riga e aggiornamento numero alunno
+                    string[] dati = sr.ReadLine().Split("\t");
+                    numeroAlunno++;
+
+                    // Controlli input
+                    if (dati.Length != 6)
+                    {
+                        Console.WriteLine($"ERRORE : Non sono presenti un totale di 6 dati nel {numeroAlunno}° alunno.");
+                        continue;
+                    }
+                    if (dati[2] != "F" && dati[2] != "M")
+                    {
+                        Console.WriteLine($"ERRORE : Genere non valido per il {numeroAlunno}° alunno.");
+                        continue;
+                    }
+                    DateOnly dataDiNascita;
+                    if (!DateOnly.TryParse(dati[3], out dataDiNascita))
+                    {
+                        Console.WriteLine($"ERRORE : Data di nascita non valida per il {numeroAlunno}° alunno.");
+                        continue;
+                    }
+
+                    // Creazione e aggiunta alunno
+                    alunni.Add(new Alunno(dati[0], dati[1], dati[2], dataDiNascita, dati[4], dati[5]));
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Memo Alunni by Fabio Fantini 4H 2024-09-20";
+
+            // Lettura input
+            LeggiAlunni();
+            Console.WriteLine();
+
+            // Stampa dati salvati
+            foreach(Alunno alunno in alunni)
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}", 
+                    alunno.GetNome(),   alunno.GetCognome(),
+                    alunno.GetGenere(), alunno.GetDataDiNascita(),
+                    alunno.GetClasse(), alunno.GetIndirizzo());
+
+
+        }
+    }
+}
