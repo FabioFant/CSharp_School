f8bfa527995003503c0b02a8992f216572e12b71
Fabio Fantini <fabiofanta07@gmail.com>
2024-11-08 00:09:47 +0100

    Aggiunto progetto di ripasso su OOP per interrogazione

---
+++ ConsoleAppRipasso/ConsoleAppRipasso/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ConsoleAppRipasso
+{
+    internal class Program
+    {
+        //INCAPSULAMENTO
+         class Dipendente
+        {
+            protected string nome;
+            public string Nome { get { return nome; } protected set { nome = value; } }
+            // Overload
+            public Dipendente(string nome) { this.nome = nome; }
+            public Dipendente()
+                : this("") { }
+            public int Ritorna(int num) { return num; }
+            public int Ritorna() { return 0; }
+
+            public void Saluta() { Console.WriteLine($"Ciao, il mio nome è {Nome}"); }
+            public virtual void Override() { Console.WriteLine("Virtual!"); }
+        }
+        //EREDIETARITA'
+        class Docente : Dipendente
+        {
+            protected string materia;
+            public string Materia { get { return materia; } protected set { materia = value; } }
+            // Overload
+            public Docente(string nome, string materia)
+                : base(nome)
+            {
+                this.materia = materia;
+            }
+            public Docente(string materia)
+                 : this("", materia) { }
+            public Docente()
+                 : this("", "") { }
+            // Override
+            public override void Override() { Console.WriteLine("Override!"); }
+        }
+
+        static void Main(string[] args)
+        {
+            //POLIMORFISMO
+            Dipendente runtime = new Docente("Dynamic", "Runtime");          // Docente --> Runtime
+            Docente compile_time = new Docente("Static", "Compile Time");    // Docente --> Compile-time
+
+            Console.WriteLine(((Docente)runtime).Materia);
+            Console.WriteLine(compile_time.Materia);
+
+            runtime.Saluta();
+            compile_time.Saluta();
+
+            Dipendente virtu = new Dipendente();
+
+            //Console.WriteLine(((Docente)virtu).Materia); NO! non si può fare il contrario, da superclasse a sottoclasse
+
+            virtu.Override();
+            runtime.Override();
+            compile_time.Override();
+        }
+    }
+}
---
+++ ConsoleAppRipasso/ConsoleAppVeicoli/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace ConsoleAppVeicoli
+{
+    internal class Program
+    {
+        interface IMotorizzato
+        {
+            public int NumeroDiRuote { get; }
+            public void AvviaMotore();
+        }
+
+        abstract class Veicolo
+        {
+            public string Marca { get; }
+            public Veicolo(string marca)
+            {
+                Marca = marca;
+            }
+            public abstract void Guida();
+            public virtual void MostraDettagli() { Console.WriteLine(Marca); }
+        }
+
+        class Auto : Veicolo, IMotorizzato
+        {
+            public int NumeroDiRuote { get; }
+            public Auto(string marca)
+                : base(marca)
+            {
+                NumeroDiRuote = 4;
+            }
+            public void AvviaMotore() { Console.WriteLine("Motore dell'auto avviato!"); }
+            public override void Guida() { Console.WriteLine("Stai guidando l'auto."); }
+            public override void MostraDettagli() { Console.WriteLine(Marca + " " + NumeroDiRuote); }
+        }
+
+        class Bicicletta : Veicolo
+        {
+            public Bicicletta(string marca)
+                : base(marca) { }
+
+            public override void Guida() { Console.WriteLine("Stai pedalando la bicicletta"); }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Veicolo> veicoli = new List<Veicolo>() { new Bicicletta("Argon 18"), new Auto("Fiat") };
+            for (int i = 0; i < veicoli.Count; i++)
+                if (veicoli[i] is IMotorizzato)
+                {
+                    IMotorizzato motorizzato = veicoli[i] as IMotorizzato;
+                    if (motorizzato != null)
+                        motorizzato.AvviaMotore();
+                }
+                else if (veicoli[i] is Bicicletta)
+                {
+                    Bicicletta bicicletta = veicoli[i] as Bicicletta;
+                    if (bicicletta != null)
+                        bicicletta.Guida();
+                }
+
+        }
+    }
+}
